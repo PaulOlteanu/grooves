60adbdc1e34797c7dc0afae60e91918d68d80aa2
Paul Olteanu <p.a.olteanu@gmail.com>
2023-03-09 14:57:41 -0500

    Remove fetcher

--- server/src/models/spotify.rs
+++ server/src/models/spotify.rs
@@ -6,12 +6,3 @@ pub struct SpotifyCreds {
     pub access_token: String,
     pub refresh_token: Option<String>,
 }
-
-impl SpotifyCreds {
-    pub fn new(access_token: String, refresh_token: Option<String>) -> Self {
-        Self {
-            access_token,
-            refresh_token,
-        }
-    }
-}
--- server/src/util/spotify/mod.rs
+++ server/src/util/spotify/mod.rs
@@ -3,10 +3,6 @@ use std::sync::Arc;
 use rspotify::sync::Mutex;
 use rspotify::{scopes, AuthCodeSpotify, Config, Credentials, OAuth, Token};
 
-use crate::models::spotify::SpotifyCreds;
-
-pub mod fetcher;
-
 /// This will return a spotify client without any token
 pub fn init_client() -> AuthCodeSpotify {
     let creds = Credentials::from_env().unwrap();
@@ -46,31 +42,5 @@ pub fn client_with_token(mut token: Token) -> AuthCodeSpotify {
 
     let mut client = AuthCodeSpotify::with_config(creds, oauth, config);
     client.token = Arc::new(Mutex::new(Some(token)));
-    client
-}
-
-pub fn init_client_with_creds(token: SpotifyCreds) -> AuthCodeSpotify {
-    let creds = Credentials::from_env().unwrap();
-
-    let scopes = scopes!(
-        "user-read-currently-playing",
-        "user-modify-playback-state",
-        "user-read-playback-state",
-        "playlist-read-private",
-        "user-read-private"
-    );
-
-    let oauth = OAuth::from_env(scopes.clone()).unwrap();
-
-    let mut client = AuthCodeSpotify::new(creds, oauth);
-    let token = Token {
-        access_token: token.access_token,
-        refresh_token: token.refresh_token,
-        scopes,
-        ..Default::default()
-    };
-
-    client.token = Arc::new(Mutex::new(Some(token)));
-
     client
 }
